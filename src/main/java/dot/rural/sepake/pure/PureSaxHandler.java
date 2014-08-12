21d21ae1320325dd16df0cc72693de496114f3fc
Niels Christensen <nchristensen@abdn.ac.uk>
2014-08-12 12:02:37 +0100

    Functional and slightly tested SAX parser for PURE projects

--- src/main/java/dot/rural/sepake/pure/PureSaxHandler.java
+++ src/main/java/dot/rural/sepake/pure/PureSaxHandler.java
@@ -2,100 +2,115 @@ package dot.rural.sepake.pure;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 import java.util.Stack;
-import java.util.TreeSet;
 
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
+/**
+ * A SAX handler that generates RDF triples from the output of
+ * the REST interface of Elsevier PURE version 4.
+ * 
+ * A PureSaxHandler acts as a Director in the Builder pattern.
+ * The Builder is a TripleSink<String, String, String>
+ */
 public final class PureSaxHandler extends DefaultHandler {
+    /** These tags contain data that we currently do not need */
     private static final List<String> IGNORE = Arrays.asList(
             "stab:associatedPublications", "stab:personsUK",
             "stab:associatedActivities",
             "stab:staffOrganisationAssociations",
             "person-template:nameVariants", "person-template:callName");
-    private final Set<String> triples = new TreeSet<String>();
+    /** Parsed triples will be added to this object. */
+    private final TripleSink<String, String, String> triples;
+    /** Records the currently open tags during parsing */
     private Stack<String> localNames = new Stack<String>();
+    /** Records all characters seen since last tag close */
     private StringBuffer text = null;
+    /** If a project is currently begin parsed, this records its RDF identifier */
     private String project = null;
+    /** If a department is currently begin parsed, this records its RDF identifier */
     private String dept = null;
+    /** If a person is currently begin parsed, this records its RDF identifier */
     private String person = null;
-
-    public void dump() {
-        for (final String triple : triples) {
-            System.out.println(triple);
-        }
+    
+    /**
+     * Registers the Builder object.
+     * @param tripleSink Parsed triples will be added to this object.
+     */
+    public PureSaxHandler(final TripleSink<String, String, String> tripleSink) {
+        this.triples = tripleSink;
     }
-
+    
     @Override
     public void startElement(String uri, String localName, String qName,
             Attributes attributes) throws SAXException {
+        //Skip processing if we're inside an ignored tag
         if ((localNames.size() > 0) && IGNORE.contains(localNames.peek())) {
             return;
         }
+        //Skip processing if we encounter an ignored tag
         if (IGNORE.contains(qName)) {
             localNames.push(qName);
             return;
         }
+        //Update parser fields
         localNames.push(qName);
         text = new StringBuffer();
-        if ("core:content".equals(qName)) {
+        if ("core:content".equals(qName)) {// This defines a new project
             project = String.format(":Project#%s",
                     attributes.getValue("uuid"));
-            triples.add(String.format("%s a prov:Organization", project));
+            triples.addTriple(project, "a", "prov:Organization");
         }
-        if ("stab1:owner".equals(qName)) {
+        if ("stab1:owner".equals(qName)) {// This defines a new department
             dept = String.format(":Department#%s",
                     attributes.getValue("uuid"));
-            triples.add(String.format("%s a prov:Organization", dept));
-            triples.add(String.format("%s :owns %s", dept, project));
+            triples.addTriple(dept, "a", "prov:Organization");
+            triples.addTriple(dept, ":owns", project);
         }
-        if ("person-template:person".equals(qName)) {
+        if ("person-template:person".equals(qName)) {// This defines a new person
             person = String.format(":Person#%s",
                     attributes.getValue("uuid"));
-            triples.add(String.format("%s a prov:Person", person));
-            triples.add(String.format("%s a foaf:Person", person));
-            triples.add(String.format("%s prov:memberOf %s", person,
-                    project));
+            triples.addTriple(person, "a", "prov:Person");
+            triples.addTriple(person, "a", "foaf:Person");
+            triples.addTriple(person, "prov:memberOf", project);
         }
     }
 
     @Override
     public void endElement(String uri, String localName, String qName)
             throws SAXException {
+        //Update tag stack
         if ((localNames.size() > 0) && qName.equals(localNames.peek())) {
             localNames.pop();
         }
+        //Skip processing if we're inside an ignored tag
         if ((localNames.size() > 0) && IGNORE.contains(localNames.peek())) {
             return;
         }
+        //Update parser field
         String fullText = text.toString().trim();
-        if (fullText.length() > 0) {
-            if ("stab1:title".equals(qName)) {
-                triples.add(String.format("%s rdfs:label \"%s\"", project,
-                        fullText));
+        if (fullText.length() > 0) {// If there was actual text, use it if the tag was relevant
+            fullText = String.format("\"%s\"", fullText);//Quote as SPARQL constant
+            if ("stab1:title".equals(qName)) {// Title of a project
+                triples.addTriple(project, "rdfs:label", fullText);
             }
             if ("organisation-template:name".equals(qName)
-                    && (localNames.contains("stab1:owner"))) {
-                triples.add(String.format("%s rdfs:label \"%s\"", dept,
-                        fullText));
+                    && (localNames.contains("stab1:owner"))) {// Name of a department
+                triples.addTriple(dept, "rdfs:label", fullText);
             }
             if ("core:firstName".equals(qName)
-                    && (localNames.contains("person-template:person"))) {
-                triples.add(String.format("%s foaf:givenName \"%s\"",
-                        person, fullText));
+                    && (localNames.contains("person-template:person"))) {// Name of a person
+                triples.addTriple(person, "foaf:givenName", fullText);
             }
             if ("core:lastName".equals(qName)
-                    && (localNames.contains("person-template:person"))) {
-                triples.add(String.format("%s foaf:familyName \"%s\"",
-                        person, fullText));
+                    && (localNames.contains("person-template:person"))) {// Name of a person
+                triples.addTriple(person, "foaf:familyName", fullText);
             }
             if ("person-template:email".equals(qName)
-                    && (localNames.contains("person-template:person"))) {
-                triples.add(String.format("%s foaf:mbox \"%s\"", person,
-                        fullText));
+                    && (localNames.contains("person-template:person"))) {// Email of a person
+                triples.addTriple(person, "foaf:mbox", fullText);
             }
         }
     }
--- src/test/java/dot/rural/sepake/pure/PureSaxHandlerTest.java
+++ src/test/java/dot/rural/sepake/pure/PureSaxHandlerTest.java
@@ -1,29 +1,30 @@
 package dot.rural.sepake.pure;
 
-import static org.junit.Assert.*;
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.xml.parsers.SAXParserFactory;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 public class PureSaxHandlerTest {
-
     @Test
-    public void testCharacters() {
-        fail("Not yet implemented");
+    public void testProjectRural() throws Exception {
+        final InputStream is = getClass().getResourceAsStream("search-projects-for-rural.xml");
+        final Set<String> output = new HashSet<String>();
+        try {
+            SAXParserFactory.newInstance().newSAXParser().parse(is, new PureSaxHandler(
+                    new TripleSink<String, String, String>() {
+                        public void addTriple(final String first, final String second, final String third) {
+                            output.add(String.format("%s %s %s", first, second, third));
+                        }
+                    }));
+        } finally {
+            is.close();
+        }
+        Assert.assertEquals(65, output.size());
+        Assert.assertTrue(output.contains(":Person#0606734d-5693-49ea-9ee3-cd4b8ddad60b foaf:givenName \"Peter\""));
     }
-
-    @Test
-    public void testDump() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testStartElementStringStringStringAttributes() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testEndElementStringStringString() {
-        fail("Not yet implemented");
-    }
-
 }
