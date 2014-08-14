5f65620ea0bbdad15334c2fb75a50255184df239
Niels Christensen <nchristensen@abdn.ac.uk>
2014-08-14 13:49:13 +0100

    Two queries updating the THML: Label and owner

--- src/main/js/sparql.js
+++ src/main/js/sparql.js
@@ -1,42 +1,51 @@
 function updateFromIri(iri) {
-  var q = $( "body" ).data("metadata").replace(/--IRI--/g, iri);
+  fuseki( "metadata", iri );
+  fuseki( "owner", iri);
+}
+
+function fuseki(template_key, iri) {
+  var q = $( "body" ).data(template_key).query.replace(/--IRI--/g, iri);
   $.ajax({
     url: "http://localhost:3030/ds/query",
     data: {
       "query" : q},
     dataType: 'json',
-    success: updateFromData
-  });
+    success: $( "body" ).data(template_key).callback
+  });  
 }
 
-function updateFromData( data ) {
-  bindings = data.results.bindings;
-  console.log(bindings);
-  for (index in bindings) {
-    if (bindings[index].relation.value === "http://www.w3.org/2000/01/rdf-schema#label") {
-      $( "#labelOfFocus" ).text(bindings[index].value.value)
-    }
-  }
+function register(templateName, lines, callback) {
+  $( "body" ).data(templateName, {"query": lines.join("\n"), "callback": callback});
 }
 
 $( document ).ready( function() {
-  $( "body" ).data("test", 
+  register(
+      "metadata", 
       [
        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
-       "SELECT * WHERE {",
+       "SELECT ?label WHERE {",
        "    BIND (<--IRI--> AS ?focus) .",
-       "    {?subject ?relation ?focus} .",
-       "    {?subject ?labelType ?subjectLabel} .",
-       "    FILTER ( isLiteral ( ?subjectLabel ) )",
+       "    {?focus rdfs:label ?label}",
        "}",
-      ].join("\n"));
-  $( "body" ).data("metadata", 
+       "LIMIT 1",
+      ],
+      function (data) {
+        $( "#labelOfFocus" ).text(data.results.bindings[0].label.value);
+      });
+  register(
+      "owner", 
       [
-       "SELECT * WHERE {",
+       "BASE <http://dot.rural/sepake/>",
+       "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
+       "SELECT ?label WHERE {",
        "    BIND (<--IRI--> AS ?focus) .",
-       "    {?focus ?relation ?value} .",
-       "    FILTER ( isLiteral ( ?value ) )",
+       "    {?owner <owns> ?focus} .",
+       "    {?owner rdfs:label ?label}",
        "}",
-      ].join("\n"));
+       "LIMIT 1",
+      ],
+      function (data) {
+        $( "#labelOfOwner" ).text(data.results.bindings[0].label.value);
+      });
   updateFromIri( "http://dot.rural/sepake/Project#e963d657-b41f-44eb-a85d-7639346b378d" );
 })
