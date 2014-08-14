1595a5402ea643888b3edbdc523e3c9fe3507a17
Niels Christensen <nchristensen@abdn.ac.uk>
2014-08-14 13:16:46 +0100

    Data now used to display stuff on screen

--- src/main/js/sparql.js
+++ src/main/js/sparql.js
@@ -1,7 +1,5 @@
-var BODY_DATA_KEY_SPARQL_TEMPLATE = "SPARQL_TEMPLATE";
-
 function updateFromIri(iri) {
-  var q = $( "body" ).data(BODY_DATA_KEY_SPARQL_TEMPLATE).replace(/--IRI--/g, iri);
+  var q = $( "body" ).data("metadata").replace(/--IRI--/g, iri);
   $.ajax({
     url: "http://localhost:3030/ds/query",
     data: {
@@ -12,11 +10,16 @@ function updateFromIri(iri) {
 }
 
 function updateFromData( data ) {
-  console.log(data);
+  bindings = data.results.bindings;
+  for (index in bindings) {
+    if (bindings[index].relation.value === "http://www.w3.org/2000/01/rdf-schema#label") {
+      $( "#labelOfFocus" ).text(bindings[index].value.value)
+    }
+  }
 }
 
 $( document ).ready( function() {
-  $( "body" ).data(BODY_DATA_KEY_SPARQL_TEMPLATE, 
+  $( "body" ).data("test", 
       [
        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
        "SELECT * WHERE {",
@@ -24,6 +27,14 @@ $( document ).ready( function() {
        "    {?subject ?relation ?focus} .",
        "    {?subject ?labelType ?subjectLabel} .",
        "    FILTER ( isLiteral ( ?subjectLabel ) )",
+       "}",
+      ].join("\n"));
+  $( "body" ).data("metadata", 
+      [
+       "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
+       "SELECT * WHERE {",
+       "    BIND (<--IRI--> AS ?focus) .",
+       "    {?focus ?relation ?value} .",
        "}",
       ].join("\n"));
   updateFromIri( "http://dot.rural/sepake/Project#e963d657-b41f-44eb-a85d-7639346b378d" );
