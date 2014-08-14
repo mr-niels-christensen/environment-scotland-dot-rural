9bd99995cf551f4e3bf0767c4b5d0a40a54ea2fd
Niels Christensen <nchristensen@abdn.ac.uk>
2014-08-14 11:00:32 +0100

    Slightly more satisfactory model for working with the SPARQL query. Sadly, the Internet will not allow us to put it in a separately loaded text file and test locally.

--- src/main/js/sparql.js
+++ src/main/js/sparql.js
@@ -1,9 +1,7 @@
+var BODY_DATA_KEY_SPARQL_TEMPLATE = "SPARQL_TEMPLATE";
+
 function updateFromIri(iri) {
-  var q = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> ";
-  q += "SELECT * WHERE {{ BIND (<" + iri + "> AS ?focus) . ";
-  q += "{?focus ?relation ?object} . FILTER ( isLiteral( ?object ) ) }";
-  q += "UNION {{?subject ?relation ?focus} . ";
-  q += "OPTIONAL {?subject rdfs:label ?subjectLabel}}}";
+  var q = $( "body" ).data(BODY_DATA_KEY_SPARQL_TEMPLATE).replace(/--IRI--/g, iri);
   $.ajax({
     url: "http://localhost:3030/ds/query",
     data: {
@@ -17,6 +15,14 @@ function updateFromData( data ) {
   console.log(data);
 }
 
-$( document ).ready(function() {
+$( document ).ready( function() {
+  $( "body" ).data(BODY_DATA_KEY_SPARQL_TEMPLATE, 
+      [
+       "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
+       "SELECT * WHERE {{ BIND (<--IRI--> AS ?focus) .",
+       "{?focus ?relation ?object} . FILTER ( isLiteral( ?object ) ) }",
+       "UNION {{?subject ?relation ?focus} .",
+       "OPTIONAL {?subject rdfs:label ?subjectLabel}}}"
+      ].join("\n"));
   updateFromIri( "http://dot.rural/sepake/Project#e963d657-b41f-44eb-a85d-7639346b378d" );
 })
