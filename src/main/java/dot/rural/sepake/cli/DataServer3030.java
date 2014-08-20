be45871c8e5573e358a36cca2468cf804a7fdb1d
Niels Christensen <nchristensen@abdn.ac.uk>
2014-08-20 14:59:21 +0100

    Silly experiment to integrate Apache Metamodel On branch 000-nc-experiments

--- src/main/java/dot/rural/sepake/cli/DataServer3030.java
+++ src/main/java/dot/rural/sepake/cli/DataServer3030.java
@@ -5,6 +5,12 @@ import java.io.InputStream;
 import javax.xml.parsers.SAXParserFactory;
 
 import org.apache.jena.fuseki.EmbeddedFusekiServer;
+import org.apache.metamodel.DataContext;
+import org.apache.metamodel.QueryPostprocessDataContext;
+import org.apache.metamodel.schema.Column;
+import org.apache.metamodel.schema.Schema;
+import org.apache.metamodel.schema.Table;
+import org.apache.metamodel.xml.XmlDomDataContext;
 
 import dot.rural.sepake.fuseki.LocalhostFusekiUpdater;
 import dot.rural.sepake.pure.PureSaxHandler;
@@ -19,6 +25,7 @@ public final class DataServer3030 {
     };
     
     public static void main(final String[] args) {
+        metamodel();
         FUSEKI.start();
         Runtime.getRuntime().addShutdownHook(FUSEKI_CLOSER);
         loadData();
@@ -29,6 +36,27 @@ public final class DataServer3030 {
         }
     }
 
+    private static void metamodel() {
+        // a DataContext for an XML file (where metadata is automatically inferred)
+        DataContext dataContext = new XmlDomDataContext(DataServer3030.class.getResource("search-projects-for-rural.xml"), true); 
+        Schema[] schemas = dataContext.getSchemas();  
+        log("Schemas");
+        for (final Schema s : schemas) {
+            log(s.toString());
+        }
+        Table[] tables = schemas[1].getTables();  
+        log("Tables");
+        for (final Table s : tables) {
+            log(s.toString());
+        }
+        Column[] columns = tables[1].getColumns();
+        log("Columns");
+        for (final Column s : columns) {
+            log(s.toString());
+        }
+        QueryPostprocessDataContext t;
+    }
+    
     private static void log(final String msg) {
         System.out.println(msg); //TODO: Use log4j
     }
