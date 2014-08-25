e2aeb540055c25c9f8047349ceafeee256776f35
Niels Christensen <nchristensen@abdn.ac.uk>
2014-08-25 12:00:39 +0100

    Display 1 person only For #2-project-view

--- src/main/js/sparql.js
+++ src/main/js/sparql.js
@@ -1,6 +1,7 @@
 function updateFromIri(iri) {
   fuseki( "metadata", iri );
   fuseki( "owner", iri);
+  fuseki( "people", iri);
 }
 
 function fuseki(template_key, iri) {
@@ -14,17 +15,25 @@ function fuseki(template_key, iri) {
   });  
 }
 
-_PREAMBLE = [
-  "BASE <http://dot.rural/sepake/>",
-  "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
-  ];
-
 function register(templateName, lines, callback) {
   $( "body" ).data(templateName, {"query": _PREAMBLE.concat(lines).join("\n"), "callback": callback});
 }
 
 $( document ).ready( function() {
-  register(
+  register_all_sparql_queries();
+  updateFromIri( "http://dot.rural/sepake/Project#e963d657-b41f-44eb-a85d-7639346b378d" );
+});
+
+_PREAMBLE = [
+             "BASE <http://dot.rural/sepake/>",
+             "PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>",
+             "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
+             "PREFIX prov: <http://www.w3.org/ns/prov#>",
+             "PREFIX foaf: <http://xmlns.com/foaf/0.1/>",
+             ];
+
+function register_all_sparql_queries() {
+register(
       "metadata", 
       [
        "SELECT ?label WHERE {",
@@ -49,5 +58,21 @@ $( document ).ready( function() {
       function (data) {
         $( "#labelOfOwner" ).text(data.results.bindings[0].label.value);
       });
-  updateFromIri( "http://dot.rural/sepake/Project#e963d657-b41f-44eb-a85d-7639346b378d" );
-})
+  register(
+      "people",
+      [
+       "SELECT ?label WHERE {",
+       "    BIND (<--IRI--> AS ?focus) .",
+       "    {?person prov:memberOf ?focus} .",
+       "    {?person rdf:type prov:Person} .",
+       "    {?person rdf:type foaf:Person} .",
+       "    {?person foaf:givenName ?given} .",
+       "    {?person foaf:familyName ?family} .",
+       "    BIND (CONCAT(?given, ' ', ?family) AS ?label)",
+       "}",
+       "LIMIT 1",
+      ],
+      function (data) {
+        $( "#listOfPeople .person" ).text(data.results.bindings[0].label.value);
+      });
+}
