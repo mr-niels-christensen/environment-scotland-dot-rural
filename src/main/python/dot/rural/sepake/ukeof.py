5c3dced30ea5cf22833295dc8f5d8422435a7457
Niels H. Christensen <nhc@mayacs.com>
2014-09-16 21:43:33 +0100

    First step towards RDFifying the rows. For #14-ukeof

--- src/main/python/dot/rural/sepake/ukeof.py
+++ src/main/python/dot/rural/sepake/ukeof.py
@@ -24,9 +24,26 @@ def sample(rows, key, value, n=10):
     print '%s=%s' % (key, value)
     for row in [row for row in rows if row[key] == value][:n]:
         print row
-        
+
+def simplify(row):
+    row['label'] = row['Title']
+    del row['Title']
+    row['comment'] = row['Description']
+    if len(row['Objectives']) > 0:
+        row['comment']  += '\nObjectives: ' + row['Objectives']
+    if len(row['Reasons for collection']) > 0:
+        row['comment']  += '\nReasons for collection: ' + row['Reasons for collection']
+    del row['Description']
+    del row['Objectives']
+    del row['Reasons for collection']
+    del row['GMES codings']
+    del row['GEOSS codings']
+    del row['ECV codings']
+    del row['Legal background']
+    return row
+    
 if __name__ == '__main__':
-    rows = [row for row in csv.DictReader(urllib2.urlopen('https://catalogue.ukeof.org.uk/api/documents?format=csv'))]
+    rows = [simplify(row) for row in csv.DictReader(urllib2.urlopen('https://catalogue.ukeof.org.uk/api/documents?format=csv'))]
     stats(rows)
     sample(rows, 'Type', 'Activity')
     sample(rows, 'Type', 'Facility')
