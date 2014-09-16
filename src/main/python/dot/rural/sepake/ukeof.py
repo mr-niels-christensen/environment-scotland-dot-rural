c17b05ad368ddcc5dc4a9c96878216f509ddf931
Niels Christensen <nchristensen@abdn.ac.uk>
2014-09-16 14:37:51 +0100

    Access the data and parse as CSV For #14-ukeof

---
+++ src/main/python/dot/rural/sepake/ukeof.py
@@ -0,0 +1,13 @@
+'''
+Created on 16 Sep 2014
+
+@author: s05nc4
+'''
+
+import csv
+import urllib2
+
+    
+if __name__ == '__main__':
+    for row in csv.reader(urllib2.urlopen('https://catalogue.ukeof.org.uk/api/documents?format=csv')):
+        print row
