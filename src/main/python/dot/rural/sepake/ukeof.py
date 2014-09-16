9f7da7d3464010d81ebf2d9fb57266e96840a144
Niels H. Christensen <nhc@mayacs.com>
2014-09-16 21:24:10 +0100

    Add samples to statistics For #14-ukeof

--- src/main/python/dot/rural/sepake/ukeof.py
+++ src/main/python/dot/rural/sepake/ukeof.py
@@ -7,10 +7,9 @@ Created on 16 Sep 2014
 import csv
 import urllib2
 
-    
-if __name__ == '__main__':
+def stats(rows):
     acc = dict()
-    for row in csv.DictReader(urllib2.urlopen('https://catalogue.ukeof.org.uk/api/documents?format=csv')):
+    for row in rows:
         for key in row:
             if key not in acc:
                 acc[key] = set()
@@ -20,4 +19,17 @@ if __name__ == '__main__':
             print '%s: %d values' % (key, len(acc[key]))
         else:
             print '%s: %s' % (key, acc[key])
-            
+
+def sample(rows, key, value, n=10):
+    print '%s=%s' % (key, value)
+    for row in [row for row in rows if row[key] == value][:n]:
+        print row
+        
+if __name__ == '__main__':
+    rows = [row for row in csv.DictReader(urllib2.urlopen('https://catalogue.ukeof.org.uk/api/documents?format=csv'))]
+    stats(rows)
+    sample(rows, 'Type', 'Activity')
+    sample(rows, 'Type', 'Facility')
+    sample(rows, 'Type', 'Network')
+    sample(rows, 'Type', 'Programme')
+    
