92d3322849e2cb188a2275a54757e66befe5e534
Niels H. Christensen <nhc@mayacs.com>
2014-09-16 20:58:56 +0100

    Fix indent For #14-ukeof

--- src/main/python/dot/rural/sepake/ukeof.py
+++ src/main/python/dot/rural/sepake/ukeof.py
@@ -11,11 +11,10 @@ import urllib2
 if __name__ == '__main__':
     acc = dict()
     for row in csv.DictReader(urllib2.urlopen('https://catalogue.ukeof.org.uk/api/documents?format=csv')):
-        if row['Type'] == 'Activity':
-            for key in row:
-                if key not in acc:
-                    acc[key] = set()
-                acc[key].add(row[key])
+        for key in row:
+            if key not in acc:
+                acc[key] = set()
+            acc[key].add(row[key])
     for key in acc:
         if len(acc[key]) > 20:
             print '%s: %d values' % (key, len(acc[key]))
