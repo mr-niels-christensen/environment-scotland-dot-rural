db0325aa19717e36f90f9d02d893eee099cdeb33
Niels Christensen <nchristensen@abdn.ac.uk>
2014-09-17 10:30:05 +0100

    Improve readability For #14-ukeof

--- src/main/python/dot/rural/sepake/ukeof.py
+++ src/main/python/dot/rural/sepake/ukeof.py
@@ -6,24 +6,28 @@ Created on 16 Sep 2014
 
 import csv
 import urllib2
+import pprint
 
 def stats(rows):
     acc = dict()
+    total = 0
     for row in rows:
+        total += 1
         for key in row:
             if key not in acc:
                 acc[key] = set()
             acc[key].add(row[key])
+    print 'Total number of rows: %d' % total
     for key in acc:
         if len(acc[key]) > 20:
             print '%s: %d values' % (key, len(acc[key]))
         else:
             print '%s: %s' % (key, acc[key])
 
-def sample(rows, key, value, n=10):
+def sample(rows, key, value, n=2):
     print '%s=%s' % (key, value)
     for row in [row for row in rows if row[key] == value][:n]:
-        print row
+        pprint.pprint(row)
 
 def simplify(row):
     row['label'] = row['Title']
