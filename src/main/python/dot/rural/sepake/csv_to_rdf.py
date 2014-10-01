8bd1faf8608d30af03aea3999381a60256748154
Niels Christensen <nchristensen@abdn.ac.uk>
2014-10-01 15:54:48 +0100

    Towards ontology for CSV For #9-semweb-model

--- src/main/python/dot/rural/sepake/csv_to_rdf.py
+++ src/main/python/dot/rural/sepake/csv_to_rdf.py
@@ -4,33 +4,86 @@ Created on 26 Sep 2014
 @author: s05nc4
 '''
 
-from rdflib import Graph, RDF, RDFS,BNode, Literal
+from rdflib import URIRef, Graph, RDF, RDFS,BNode, Literal
 import csv
 import urllib2
-from dot.rural.sepake.ontology import SEPAKE
+
+RDF_NAME = object()
+
+class MyMeta(type):
+    def __getattribute__(self, name):
+        x = type.__getattribute__(self, name)
+        if x is RDF_NAME:
+            return URIRef(type.__getattribute__(self, 'BASE_URI') + '#' + name)
+        else:
+            return x
+        
+def namespace(base_uri):
+    def class_rebuilder(cls):
+        class NewClass(cls):
+            __metaclass__ = MyMeta
+            BASE_URI = base_uri
+        return NewClass
+    return class_rebuilder
+
+@namespace('http://dot.rural/sepake/csv')
+class CSV(object):
+    Import = RDF_NAME
+    Row = RDF_NAME
+    Cell = RDF_NAME
+    fieldName = RDF_NAME
+    fieldValue = RDF_NAME
+    
 
 class CsvGraph(Graph):
     def __init__(self, url):
         super(CsvGraph, self).__init__()
-        self._node = BNode()
-        self.add((self._node, RDF.type, SEPAKE.CsvImport))
-        self._load_csv_url(url)
-
-    def _load_csv_url(self, url):
-        for row in csv.DictReader(urllib2.urlopen(url)):
+        #ID of this import operation
+        self._ROOT_ID = BNode()
+        #Type of this import operation
+        self.add((self._ROOT_ID, RDF.type, CSV.Import))
+    
+    def read_url(self, url):
+        '''Loads a CSV file from a URL and adds each row
+        '''
+        self.read(urllib2.urlopen(url)) #TODO add PROV
+        
+    def read(self, csv_input):
+        '''Parses a CSV file and adds each row
+        '''
+        #TODO add PROV
+        for row in csv.DictReader(csv_input):
             self._add_csv_row(row)
             
     def _add_csv_row(self, row):
-        row_node = BNode()
-        self.add((row_node, RDF.type, SEPAKE.CsvRow))
-        self.add((self._node, RDFS.member, row_node))
+        '''Adds a CSV row to the graph in the following style:
+           ROOT_ID  -->  row_id --> cell_id_0 --> Heading_0
+                              |              \--> Value_0
+                              |---> cell_id_1 ...
+                               ...
+           The link from ROOT_ID to row_id is an rdfs:member
+           The link from row_id to cell_id is also an rdfs:member
+           Headings and values are literals, linked to by specific properties in our ontology.
+        '''
+        #ID of this row
+        row_id = BNode()
+        #Type of a CSV row
+        self.add((row_id, RDF.type, CSV.Row))
+        #ROOT_ID  -->  row_id 
+        self.add((self._ROOT_ID, RDFS.member, row_id))
         for heading, value in row.iter_items():
+            #ID of this cell
             cell_node = BNode()
-            self.add(row_node, RDFS.member, cell_node)
-            self.add((cell_node, SEPAKE.hasCSVHeading, Literal(heading)))
-            self.add((cell_node, SEPAKE.hasCSVValue, Literal(value)))
+            #Type of a CSV cell
+            self.add((row_id, RDF.type, CSV.Cell))
+            #row_id --> cell_id
+            self.add(row_id, RDFS.member, cell_node)
+            #cell_id --> Heading
+            self.add((cell_node, CSV.fieldName, Literal(heading)))
+            #cell_id --> Value
+            self.add((cell_node, CSV.fieldValue, Literal(value)))
 
-
-        
+if __name__ == '__main__':
+    print repr(CSV.Import)
         
         
--- src/main/python/dot/rural/sepake/ontology.py
+++ src/main/python/dot/rural/sepake/ontology.py
@@ -4,11 +4,11 @@ Created on 16 Sep 2014
 @author: s05nc4
 '''
 
-from rdflib import Graph, Namespace, RDF, RDFS, URIRef
+from rdflib import Graph, Namespace, RDFS, URIRef
 from rdflib.plugins.stores.sparqlstore import SPARQLUpdateStore
 from rdflib.namespace import FOAF, ClosedNamespace
 
-SEPAKE = ClosedNamespace(uri = URIRef('http://dot.rural/sepake/'),
+ONTOLOGY = ClosedNamespace(uri = URIRef('http://dot.rural/sepake/'),
                          terms = ["PureProject", 
                                   "PureDepartment", 
                                   "PurePerson", 
@@ -19,38 +19,34 @@ SEPAKE = ClosedNamespace(uri = URIRef('http://dot.rural/sepake/'),
                                   ])
 PROV  = Namespace('http://www.w3.org/ns/prov#')
 
-
-class OntologyLoader(Graph):
+class OntologyGraph(Graph):
     '''Class for creating RDF triples for this project's ontology.
        TODO: Discuss use of singleton properties? http://mor.nlm.nih.gov/pubs/pdf/2014-www-vn.pdf
     '''
     def __init__(self):
-        super(OntologyLoader, self).__init__()
-        self.add((SEPAKE.PureProject, RDFS.subClassOf, PROV.Organization))
-        self.add((SEPAKE.PureProject, RDFS.subClassOf, FOAF.Organization))
-        self.add((SEPAKE.PureProject, RDFS.subClassOf, PROV.Activity))
-        self.add((SEPAKE.PureDepartment, RDFS.subClassOf, PROV.Organization))
-        self.add((SEPAKE.PureDepartment, RDFS.subClassOf, FOAF.Organization))
-        self.add((SEPAKE.PurePerson, RDFS.subClassOf, PROV.Person))
-        self.add((SEPAKE.PurePerson, RDFS.subClassOf, FOAF.Person))
-        self.add((SEPAKE.UKEOFActivity, RDFS.subClassOf, PROV.Activity))
-        self.add((SEPAKE.UKEOFOrganisation, RDFS.subClassOf, PROV.Organization))
-        self.add((SEPAKE.UKEOFOrganisation, RDFS.subClassOf, FOAF.Organization))
-        self.add((SEPAKE.CrawlOperation, RDFS.subClassOf, PROV.Activity))
-        self.add((SEPAKE.contextualStatementOf, RDFS.subPropertyOf, RDF.type))
+        super(OntologyGraph, self).__init__()
+        self.add((ONTOLOGY.PureProject, RDFS.subClassOf, PROV.Organization))
+        self.add((ONTOLOGY.PureProject, RDFS.subClassOf, FOAF.Organization))
+        self.add((ONTOLOGY.PureProject, RDFS.subClassOf, PROV.Activity))
+        self.add((ONTOLOGY.PureDepartment, RDFS.subClassOf, PROV.Organization))
+        self.add((ONTOLOGY.PureDepartment, RDFS.subClassOf, FOAF.Organization))
+        self.add((ONTOLOGY.PurePerson, RDFS.subClassOf, PROV.Person))
+        self.add((ONTOLOGY.PurePerson, RDFS.subClassOf, FOAF.Person))
+        self.add((ONTOLOGY.UKEOFActivity, RDFS.subClassOf, PROV.Activity))
+        self.add((ONTOLOGY.UKEOFOrganisation, RDFS.subClassOf, PROV.Organization))
+        self.add((ONTOLOGY.UKEOFOrganisation, RDFS.subClassOf, FOAF.Organization))
+        self.add((ONTOLOGY.CrawlOperation, RDFS.subClassOf, PROV.Activity))
+        self.add((ONTOLOGY.contextualStatementOf, RDFS.subPropertyOf, RDFS.subPropertyOf))
         
-    def flush(self, other):
-        #TODO Use += but it does not seem to work unless context_aware...which does not seem to work with Fuseki
-        print 'Flushing...'
-        for triple in self:
-            print '.',
-            other.add(triple)
-        print 'Flushed'
-
 if __name__ == '__main__':
-    ont = OntologyLoader()
+    ont = OntologyGraph()
     print ont.serialize(format='turtle')
     remote = SPARQLUpdateStore(context_aware = False)
     remote.open(("http://localhost:3030/ds/query", "http://localhost:3030/ds/update"))
-    ont.flush(remote)
+    #TODO Use += but it does not seem to work unless context_aware...which does not seem to work with Fuseki
+    print 'Flushing...'
+    for triple in ont:
+        print '.',
+        remote.add(triple)
+    print 'Flushed'
     
