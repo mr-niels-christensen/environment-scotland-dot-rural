73b94f5275c3b7dd6b3eb9c6d343ffbafcd7b839
Niels H. Christensen <nhc@mayacs.com>
2014-10-20 21:48:44 +0100

    Preciser testing of PURE parsing For #9-semweb-model

--- src/test/python/dot/rural/sepake/test_pure.py
+++ src/test/python/dot/rural/sepake/test_pure.py
@@ -15,7 +15,7 @@ from dot.rural.sepake.pure import CONSTRUCT_PROJECT
 class Test(unittest.TestCase):
     def testConstructProject(self):
         with open('src/main/resources/dot/rural/sepake/cli/search-projects-for-rural.xml') as f:
-            g = XMLGraph(f, 
+            x = XMLGraph(f, 
                          delete_nodes = ['stab:associatedPublications',
                                          'stab:associatedActivities',
                                          'stab:personsUK',
@@ -25,11 +25,14 @@ class Test(unittest.TestCase):
                          namespaces = {'stab' : 'http://atira.dk/schemas/pure4/model/base_uk/project/stable',
                                        'personstab' : 'http://atira.dk/schemas/pure4/model/base_uk/person/stable',
                                        'person-template' : 'http://atira.dk/schemas/pure4/model/template/abstractperson/stable'})
-            triples = Graph()
-            triples += g.query(CONSTRUCT_PROJECT)
-            self.assertEquals(3, len(triples))
+            g = Graph()
+            g += x.query(CONSTRUCT_PROJECT)
+            self.assertEquals(3, len(g))
             proj = URIRef(u'http://dot.rural/sepake/PureProject#e963d657-b41f-44eb-a85d-7639346b378d')
-            self.assertEquals(SEPAKE.PureProject, triples.value(proj, RDF.type, any = False))
+            self.assertEquals(SEPAKE.PureProject, g.value(proj, RDF.type, any = False))
+            self.assertEquals(Literal('RURAL DIGITAL ECONOMY RESEARCH HUB'),
+                              g.value(proj, RDFS.label, any = False))
+            self.assertTrue(str(g.value(proj, SEPAKE.htmlDescription, any = False)).startswith('One of the three'))
             
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
