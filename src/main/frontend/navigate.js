ef7c85ce01d711563e536fffcbf8d5d1117e13ae
Niels Christensen <nchristensen@abdn.ac.uk>
2014-10-29 13:38:31 +0000

    Don't edit inline For #25-qa-prototype

--- src/main/frontend/navigate.js
+++ src/main/frontend/navigate.js
@@ -13,7 +13,10 @@ function indexJsInit() {
   });
   $( "#askButton" ).click(function() {
     //var url = encodeURI('https://twitter.com/intent/tweet?screen_name=niels_may_acs&text=' + $( '#questionText' ).val());
-    var url = encodeURI('http://earthscience.stackexchange.com/questions/ask?tags=environment scotland&title=' + $( '#questionText' ).val());
+    var url = encodeURI(
+        'http://earthscience.stackexchange.com/questions/ask?tags=environment scotland&title=' 
+        + $( '#questionText' ).text()
+    );
     $( this ).attr( "href", url );
   });
 }
