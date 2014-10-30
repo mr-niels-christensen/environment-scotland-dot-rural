95705afa5668b7da0ad192fc1deda7bbd0c15133
Niels H. Christensen <nhc@mayacs.com>
2014-10-30 20:53:19 +0000

    Removed obsolete code For #26-data-model-visualization

--- src/main/frontend/navigate.js
+++ src/main/frontend/navigate.js
@@ -1,14 +1,6 @@
 function indexJsInit() {
   $('#search').one( "preloaded", initSearch);
   fuseki( "searchables", "");
-  $( "#askButton" ).click(function() {
-    //var url = encodeURI('https://twitter.com/intent/tweet?screen_name=niels_may_acs&text=' + $( '#questionText' ).val());
-    var url = encodeURI(
-        'http://earthscience.stackexchange.com/questions/ask?tags=environment scotland&title=' 
-        + $( '#questionText' ).text()
-    );
-    $( this ).attr( "href", url );
-  });
 }
 
 function initSearch(event, data) {
