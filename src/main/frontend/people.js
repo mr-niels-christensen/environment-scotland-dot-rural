7af8ad9b5670be59ba77c355bc3d519d5fe95c4c
Niels H. Christensen <nhc@mayacs.com>
2014-10-30 20:18:53 +0000

    First step in required refactoring: Add people.js For #26-data-model-visualization

---
+++ src/main/frontend/people.js
@@ -0,0 +1,12 @@
+$( document ).ready( function() {
+  $( ".x10" ).each( function(index, dom_element) {
+    var new_elems = [];
+    var n_copies = 9;
+    for (i = 0; i < n_copies; i++) {
+      new_elems.push($(dom_element).children().clone());
+    }
+    for (i = 0; i < n_copies; i++) {
+      $(dom_element).append(new_elems[i]);
+    }
+  });
+});
