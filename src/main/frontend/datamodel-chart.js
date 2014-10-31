29a64944382ee2f2db3a8151e69f8c55905dbcd2
Niels Christensen <nchristensen@abdn.ac.uk>
2014-10-31 13:57:17 +0000

    WordTree does not work. Revert to simpler solution. For #26-data-model-visualization

--- src/main/frontend/datamodel-chart.js
+++ src/main/frontend/datamodel-chart.js
@@ -3,12 +3,13 @@ google.setOnLoadCallback( function() {
     $( document ).ready( function() {
         sparql(
                 [
-                 "SELECT ?p ?y WHERE {",
+                 "SELECT ?rdftype (COUNT(?x) AS ?xs) WHERE {",
                  "  {",
-                 "    {?x rdf:type <UKEOFActivity>} .",
-                 "    {?x ?p ?y} .",
+                 "    {?x rdf:type ?rdftype} .",
                  "  }",
                  "}",
+                 "GROUP BY ?rdftype",
+                 "ORDER BY DESC(?xs)"
                 ],
                 "",
                 _updateDatamodelChartFromJson
@@ -17,44 +18,25 @@ google.setOnLoadCallback( function() {
 });
 
 function _updateDatamodelChartFromJson(response) {
-    dataModelBegin();
     $.each(response.results.bindings, function(index, binding){
       values = _valuesOfSparqlBinding(binding);
-      dataModelData(values);
+      var brief = values.rdftype.split('/').pop();
+      $( "#chartPanel" ).append( "<h2>" + brief + ": " + values.xs + " instances</h2><p id='" + brief +"'></p>");
+      
     });
-    dataModelEnd();        
 };
 
-//TODO: Wrap this functionality as an object
-var tbl;
-var chart;
-function dataModelBegin() {
-    //Initialize tbl (internal data table drawn on the chart) and chart
-    tbl = new google.visualization.DataTable();
-    tbl.addColumn('string', 'Phrases');
-    chart = new google.visualization.WordTree(document.getElementById('chartPanel'));
-}
-function dataModelEnd() {
-    chart.draw(tbl, 
-            { height : 1000,
-              width : 1000,
-              wordtree: {
-                format: 'implicit',
-                word : 'x'
-                        }
-            });
-}
-function dataModelData( values ) {
-  var p = values.p.split('/').pop();
-  var y;
-  if (values.y.indexOf('/') > -1) {
-    y = values.y.split('/').pop();
-  } else {
-    y = 'data';
-  };
-  var text = 'x ' + p + ' ' + y
-  console.log(text);
-  tbl.addRow(
-    [ text
-    ]);
+function _example(rdftype, id) {
+  sparql([
+          "SELECT DISTINCT ?rdftype WHERE {",
+          "  {",
+          "    {?x rdf:type ?rdftype} .",
+          "  }",
+          "}",
+          "ORDER BY ?rdftype"
+          ],
+          values.rdftype,
+          function(response) {
+            //TODO
+  });
 }
