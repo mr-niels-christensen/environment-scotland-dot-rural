09dde5da1a21b9bc3ce3764c3beb8acdf70c9d3a
Niels H. Christensen <nhc@mayacs.com>
2014-11-25 13:48:43 +0000

    Hide ask panel, unless specifically asked for For #master

--- src/main/frontend/ask.js
+++ src/main/frontend/ask.js
@@ -1,3 +1,9 @@
+//For alternative methods, see http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+function getParameterByName(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
 $( document ).ready( function() {
   $( "#askButton" ).click(function() {
     //var url = encodeURI('https://twitter.com/intent/tweet?screen_name=niels_may_acs&text=' + $( '#questionText' ).val());
@@ -7,4 +13,8 @@ $( document ).ready( function() {
     );
     $( this ).attr( "href", url );
   });
+  var ask = getParameterByName('ask');
+  if (ask == 'on') {
+    $( '#askPanel' ).show();  
+  };
 });
