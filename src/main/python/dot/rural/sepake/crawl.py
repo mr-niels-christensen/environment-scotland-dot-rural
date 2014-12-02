827a82cf0d9e980028cd7d7aca53b0051c535528
Niels Christensen <nchristensen@abdn.ac.uk>
2014-12-02 11:59:17 +0000

    Pass query parameters to action function For #7-pure-papers

--- src/main/python/dot/rural/sepake/crawl.py
+++ src/main/python/dot/rural/sepake/crawl.py
@@ -14,23 +14,25 @@ def route():
 
 class _CrawlHandler(webapp2.RequestHandler):
     def get(self, action):
-        g = Graph(store = NDBStore(identifier = self.request.get('graphid')))
-        _ACTIONS[action](g)
+        _ACTIONS[action](**self.request.GET)
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('OK')
 
-def _load_pure_data(g):
+def _graph(graphid):
+    return Graph(store = NDBStore(identifier = graphid))
+
+def _load_pure_data(graphid):
     logging.info('Loading and processing data from PURE...')
     from dot.rural.sepake.pure import university_of_aberdeen
-    copy_graph_to_graph(university_of_aberdeen(), g)
+    copy_graph_to_graph(university_of_aberdeen(), _graph(graphid))
     
-def _load_ukeof_data(g):
+def _load_ukeof_data(graphid):
     logging.info('Loading and processing data from UKEOF...')
     from dot.rural.sepake.ukeof import ukeof_graphs
-    copy_graphs_to_graph(ukeof_graphs(), g)
+    copy_graphs_to_graph(ukeof_graphs(), _graph(graphid))
 
-def _crawl_pure_oai(g):
-    pass
+def _crawl_pure_oai(graphid, location, pureset):
+    logging.debug('{} <= http://{}/ws/oai?verb=ListRecords&set={}&metadataPrefix=oai_dc'.format(graphid, location, pureset))
 
 _ACTIONS = { 'pure.projects.aberdeen' : _load_pure_data,
              'ukeof' :                  _load_ukeof_data,
