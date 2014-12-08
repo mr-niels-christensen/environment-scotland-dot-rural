dcd896c58b106a9b785c9e066940a86a82f005ee
Niels H. Christensen <nhc@mayacs.com>
2014-12-08 21:46:05 +0000

    Removed obsolete code For #tidyup

--- src/main/python/dotruralsepake/rdf/utils.py
+++ src/main/python/dotruralsepake/rdf/utils.py
@@ -4,38 +4,7 @@ Created on 20 Oct 2014
 @author: s05nc4
 '''
 
-import logging
-import time
-from rdflib import Graph, URIRef
-
-_PERCENTAGES = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 101]
-
-def copy_graph_to_graph(src_graph, dest_graph, use_multiadd = True):
-    start = time.time()
-    length = len(src_graph)
-    logging.info('Storing %d triples...' % length)
-    if use_multiadd:
-        dest_graph += src_graph
-        logging.debug('Done, storing took %f seconds' % (time.time() - start))
-        return
-    checkpoints = [((x * length) / 100, '%0d%%' % x) for x in _PERCENTAGES]
-    total = 0
-    for triple in src_graph:
-        try:
-            dest_graph.add(triple)
-        except Exception as e:
-            logging.warn('Failed to add %s: %s' % (triple, e))
-        total += 1
-        if total > checkpoints[0][0]:
-            logging.debug('%s, %f seconds' % (checkpoints[0][1], time.time() - start))
-            checkpoints = checkpoints[1:]
-    logging.info('Done')
-
-def copy_graphs_to_graph(src_graphs, dest_graph, use_multiadd = True):
-    union_graph = Graph()
-    for g in src_graphs:
-        union_graph += g
-    copy_graph_to_graph(union_graph, dest_graph, use_multiadd)
+from rdflib import URIRef
 
 '''Used as a marker for members that are to be treated as RDF names.'''
 RDF_NAME = object()
