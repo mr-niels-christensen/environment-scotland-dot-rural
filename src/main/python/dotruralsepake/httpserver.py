27fa1d58a677250b1142d049848f9cfe5de99eb3
Niels Christensen <nchristensen@abdn.ac.uk>
2014-12-08 14:35:52 +0000

    Tidy up httpserver For #tidyup

--- src/main/python/dotruralsepake/httpserver.py
+++ src/main/python/dotruralsepake/httpserver.py
@@ -1,34 +1,10 @@
-import logging
 import webapp2
-from rdflib import Graph
-from rdflib_appengine.ndbstore import NDBStore
-from time import time
-from dotruralsepake.crawl import route
+from dotruralsepake import crawl_handler
+from dotruralsepake import query_handler
 
-class _QueryJson(webapp2.RequestHandler):
-    def get(self, graphid):
-        logging.debug('Responding to {}'.format(self.request.get('name')))
-        begin = time()
-        #Access-Control-Allow-Origin: *
-        self.response.headers['Access-Control-Allow-Origin'] = '*'
-        self.response.headers['Content-Type'] = 'application/sparql-results+json; charset=utf-8'
-        self.response.write(query(graphid,
-                                  self.request.get('query'),
-                                  self.request.get('name')))
-        
-        logging.debug('Responded to {} in {:.3f} seconds'.format(self.request.get('name'), time() - begin))
 
 application = webapp2.WSGIApplication([
-    webapp2.Route(r'/sparql/<graphid>/query.json', handler=_QueryJson, name='sparql'),
-    route(),
+    query_handler.route(),
+    crawl_handler.route(),
 ], debug=True) #debug=true means stack traces in browser
 
-def query(graphid, q, name):
-    store = NDBStore(identifier = graphid, configuration = {'log' : True})
-    store.log(name)
-    try:
-        response = Graph(store = store).query(q).serialize(format='json')
-        return response
-    finally:
-        store.flush_log(logging.DEBUG)
-    
---
+++ src/main/python/dotruralsepake/query_handler.py
@@ -0,0 +1,38 @@
+import logging
+import webapp2
+from rdflib import Graph
+from rdflib_appengine.ndbstore import NDBStore
+from time import time
+'''
+Created on 8 Dec 2014
+
+@author: s05nc4
+'''
+
+def route():
+    return webapp2.Route(r'/sparql/<graphid>/query.json', handler=_QueryJson, name='sparql')
+
+class _QueryJson(webapp2.RequestHandler):
+    def get(self, graphid):
+        logging.debug('Responding to {}'.format(self.request.get('name')))
+        begin = time()
+        #Access-Control-Allow-Origin: *
+        self.response.headers['Access-Control-Allow-Origin'] = '*'
+        self.response.headers['Content-Type'] = 'application/sparql-results+json; charset=utf-8'
+        self.response.write(query(graphid,
+                                  self.request.get('query'),
+                                  self.request.get('name')))
+        
+        logging.debug('Responded to {} in {:.3f} seconds'.format(self.request.get('name'), time() - begin))
+
+def query(graphid, q, name):
+    store = NDBStore(identifier = graphid, configuration = {'log' : True})
+    store.log(name)
+    try:
+        response = Graph(store = store).query(q).serialize(format='json')
+        return response
+    finally:
+        store.flush_log(logging.DEBUG)
+    
+
+
