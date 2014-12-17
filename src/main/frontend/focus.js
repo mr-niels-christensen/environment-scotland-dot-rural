256c07d4ba6702fd5b7f6d1ea9dc958bfb4a2499
Niels Christensen <nchristensen@abdn.ac.uk>
2014-12-17 15:31:42 +0000

    Fixed bug found by John Dawson: Title can be HTML

--- src/main/frontend/focus.js
+++ src/main/frontend/focus.js
@@ -22,7 +22,7 @@ var _predicate_to_action = {
   "http://dot.rural/sepake/ownedBy" :
     _updateOwnerFromIri,
   "http://www.w3.org/2000/01/rdf-schema#label" : 
-    function( y ){ $( ".labelOfFocus" ).text(y) },
+    function( y ){ $( ".labelOfFocus" ).html(y) },
   "http://dot.rural/sepake/htmlDescription" : 
     function( y ){ $( "#descriptionOfFocus" ).html(y || "(No summary)") },
   "http://xmlns.com/foaf/0.1/homepage" : 
