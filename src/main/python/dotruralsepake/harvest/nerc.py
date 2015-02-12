e852606683ec8a2370665fbfde41aee17060f8c8
Niels Christensen <nchristensen@abdn.ac.uk>
2015-02-12 11:44:12 +0000

    Make sure ID of NERC data set is a hex digit as required for indexing. Also add homepage link. For #27-nerc

--- src/main/python/dotruralsepake/harvest/nerc.py
+++ src/main/python/dotruralsepake/harvest/nerc.py
@@ -7,15 +7,14 @@ Created on 20 Oct 2014
 '''
 
 from dotruralsepake.rdf.ontology import SEPAKECODE
-import logging
-from dotruralsepake.rdf.ontology import PROV
 
-from rdflib.namespace import RDF, RDFS, DC
+from rdflib.namespace import RDF, RDFS, DC, FOAF
 from rdflib import URIRef, Literal
 from dotruralsepake.rdf.ontology import SEPAKE
 from dotruralsepake.rdf.xml_to_rdf import XMLGraph
 from datetime import datetime
 import urllib2
+import logging
 
 NERC_CODE_URI = 'http://dot.rural/sepake/codeNercRSS'
 
@@ -31,19 +30,24 @@ def nerc_task_from_url(url):
     #Loop over each item in the RSS feed:
     for item in xml_graph.objects(URIRef('#rss/channel'), URIRef('#item')):
         text_link = xml_graph.value(subject = item, predicate = URIRef('#link') / RDF.value)
+        if not text_link.endswith('.xml'):
+            logging.info('Skipping {} from NERC because it does not end with XML as expected'.format(text_link))
+            continue
+        id = URIRef(text_link[:-4]) #Strip '.xml' to end with a more or less random digit
         url_link = URIRef(text_link)
-        yield list(_triples_for_rss_item(item, url_link, xml_graph))
+        yield list(_triples_for_rss_item(item, id, url_link, xml_graph))
 
-def _triples_for_rss_item(item, link, xml_graph):
-    yield (link, RDF.type, SEPAKE.NERCDataSet)
-    yield (link, RDFS.label, 
+def _triples_for_rss_item(item, id, link, xml_graph):
+    yield (id, RDF.type, SEPAKE.NERCDataSet)
+    yield (id, FOAF.homepage, link)
+    yield (id, RDFS.label, 
            xml_graph.value(subject = item, 
                            predicate = URIRef('#title') / RDF.value))
-    yield (link, SEPAKE.htmlDescription, 
+    yield (id, SEPAKE.htmlDescription, 
            xml_graph.value(subject = item, 
                            predicate = URIRef('#description') / RDF.value))
     #Example date from the RSS feed: 'Wed, 21 Jan 2015 00:00:00 GMT'
     text_date = xml_graph.value(subject = item, 
                                 predicate = URIRef('#pubDate') / RDF.value)
     python_datetime = datetime.strptime(text_date, '%a, %d %b %Y %X GMT')
-    yield (link, DC.issued, Literal(python_datetime))
+    yield (id, DC.issued, Literal(python_datetime))
