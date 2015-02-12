2cd2037aa9467cfeafb88d5b143d418b59d72b8e
Niels Christensen <nchristensen@abdn.ac.uk>
2015-02-12 11:05:19 +0000

    More reabable NERC implementation For #27-nerc

--- src/main/python/dotruralsepake/harvest/nerc.py
+++ src/main/python/dotruralsepake/harvest/nerc.py
@@ -20,26 +20,30 @@ import urllib2
 NERC_CODE_URI = 'http://dot.rural/sepake/codeNercRSS'
 
 def nerc_task_from_url(url):
+    '''Reads an RSS feed from NERC and describes the found datasets as triples.
+       @param url The RSS feed url as a string, i.e. 'http://data-search.nerc.ac.uk/search/full/catalogue.rss?'+parameters
+       @return This function is a generator yielding lists of triples
+    '''
+    #Timestamp marking that this task has been executed
     yield [(URIRef(url), SEPAKE.wasDetailedAtTime, Literal(datetime.utcnow()))]
     xml_input = urllib2.urlopen(url, timeout = 30)
     xml_graph = XMLGraph(xml_input)
+    #Loop over each item in the RSS feed:
     for item in xml_graph.objects(URIRef('#rss/channel'), URIRef('#item')):
-        yield _triples_for(item, xml_graph)
+        text_link = xml_graph.value(subject = item, predicate = URIRef('#link') / RDF.value)
+        url_link = URIRef(text_link)
+        yield list(_triples_for_rss_item(item, url_link, xml_graph))
 
-def _triples_for(item, xml_graph):
-    link = URIRef(xml_graph.value(subject = item, predicate = URIRef('#link') / RDF.value))
-    triples = [(link, RDF.type, SEPAKE.NERCDataSet)]
-    triples.append((link, 
-                    RDFS.label, 
-                    xml_graph.value(subject = item, 
-                                    predicate = URIRef('#title') / RDF.value)))
-    triples.append((link, 
-                    SEPAKE.htmlDescription, 
-                    xml_graph.value(subject = item, 
-                                    predicate = URIRef('#description') / RDF.value)))
-    pubDate = datetime.strptime(xml_graph.value(subject = item, 
-                                predicate = URIRef('#pubDate') / RDF.value), '%a, %d %b %Y %X GMT')
-    triples.append((link, 
-                    DC.issued, 
-                    Literal(pubDate)))
-    return triples
+def _triples_for_rss_item(item, link, xml_graph):
+    yield (link, RDF.type, SEPAKE.NERCDataSet)
+    yield (link, RDFS.label, 
+           xml_graph.value(subject = item, 
+                           predicate = URIRef('#title') / RDF.value))
+    yield (link, SEPAKE.htmlDescription, 
+           xml_graph.value(subject = item, 
+                           predicate = URIRef('#description') / RDF.value))
+    #Example date from the RSS feed: 'Wed, 21 Jan 2015 00:00:00 GMT'
+    text_date = xml_graph.value(subject = item, 
+                                predicate = URIRef('#pubDate') / RDF.value)
+    python_datetime = datetime.strptime(text_date, '%a, %d %b %Y %X GMT')
+    yield (link, DC.issued, Literal(python_datetime))
