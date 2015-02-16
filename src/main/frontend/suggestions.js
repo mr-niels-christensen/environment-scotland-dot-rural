9fce1dce74fc52878ca2318d28b54660aa612c46
Niels Christensen <nchristensen@abdn.ac.uk>
2015-02-16 14:27:23 +0000

    Moved search history out of suggestion and into separate file For #56-clarify-js

--- src/main/frontend/suggestions.js
+++ src/main/frontend/suggestions.js
@@ -64,29 +64,3 @@ var substringMatcher = function(projs) {
     cb(matches);
   };
 };
-
-function _updateHistoryFromHashChange(event) {
-  var recent = JSON.parse($.localStorage.getItem('search_history_recent_list')) || [];
-  if (event.getState( 'query' )) {
-    var new_len = recent.unshift(event.getState( 'query' ));
-    if (new_len > 10) {
-      recent.pop();
-    };
-    $.localStorage.setItem('search_history_recent_list', JSON.stringify(recent));
-  };
-  $( document ).ready( function() {
-    $ ( "#historyTable .dynamicrow" ).remove();
-    $.each(recent, function(index, term){
-      $( "#historyTable tr:last" ).after( "<tr class='searchTermRow dynamicrow'></tr>" );
-      $( "#historyTable tr:last" ).append( "<td>" + term + "</td>" );
-    });
-    $( "#historyTable .searchTermRow" ).on( 'click', function() {
-      var term = $( this ).find( 'td' ).text();
-      var link_url = jQuery.param.fragment( '/search.html', {'query' : term} );
-      document.location.href = link_url;
-  });
-
-  });
-}
-
-$(window).bind( 'hashchange', _updateHistoryFromHashChange);
