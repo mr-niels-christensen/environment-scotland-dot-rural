fcacebad26006081424c478353c91f4b0e575f7d
Vincent Bodinier <vbodinier@gmail.com>
2015-02-18 13:43:19 +0000

    metrics js to debug for #58

--- src/main/frontend/metrics-sparql.js
+++ src/main/frontend/metrics-sparql.js
@@ -1,16 +1,32 @@
-sparql("metrics",
+/*sparql("metrics",
         ["PREFIX met: <http://dot.rural/sepake/metrics/>", 
 		 "SELECT (COUNT(*) AS ?count) WHERE {",
-		"{BIND ( <http://dot.rural/sepake/PurePublication#d5408bac-08d0-4195-9d6b-659e9394ada2> AS ?x)}.",
-		"{?x met:focushit ?y .}",
+		"{BIND ( <http://dot.rural/sepake/PureProject#86ae16d9-54bb-46c6-93b1-34c9738951b6> AS ?x)}.",
+		"{?x met:focushit ?y}",
 		"} GROUP BY ?x"
         ],
         "",
         _updateMetricPanelFromJson
     );
-
+*/	
+sparql("metricsTitle",
+		 "SELECT (?label AS ?title)  WHERE {",
+		"{BIND ( <http://dot.rural/sepake/PureProject#86ae16d9-54bb-46c6-93b1-34c9738951b6> AS ?x)}.",
+		"{?x rdfs:label ?label}",
+		"}"
+        ],
+        "",
+        _updateMetricPanelTitleFromJson
+    );
+	
+function _updateMetricPanelTitleFromJson(response) {
+  var sparqlBinding = response.results.bindings[0];
+  var parsed = _valuesOfSparqlBinding( sparqlBinding );
+  $('#metricsPanel').append(parsed.title);
+};
+/*
 function _updateMetricPanelFromJson(response) {
   var sparqlBinding = response.results.bindings[0];
   var parsed = _valuesOfSparqlBinding( sparqlBinding );
   $('#metricsPanel').append('SUCCESS' + parsed.count);
-};
+};*/
