d806952aaf6ab88e25c12deab5446ae80dd7f929
Niels Christensen <nchristensen@abdn.ac.uk>
2015-03-10 15:59:29 +0000

    More table indexing job For #65-indexing

--- src/main/dotruralsepake/search/index.py
+++ src/main/dotruralsepake/search/index.py
@@ -3,47 +3,50 @@ Created on 9 Dec 2014
 
 @author: Niels Christensen
 '''
-from rdflib import Graph, Literal
+from rdflib import Graph, Literal, RDFS
+from rdflib.namespace import FOAF
 from rdflib_appengine.ndbstore import NDBStore
 from rdflib.plugins.sparql import prepareQuery
 from google.appengine.api import search
 import logging
 from dotruralsepake.store import connect
 from dotruralsepake.rdf.utils import prepareQuery
+from dotruralsepake.rdf.ontology import SEPAKE, SEPAKEMETRICS, PROV
+from itertools import izip_longest
+from collections import Counter
 
-_DOCUMENTS = '''
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
-PREFIX sepake: <http://dot.rural/sepake/>
-PREFIX sepakemetrics: <http://dot.rural/sepake/metrics/>
-PREFIX prov: <http://www.w3.org/ns/prov#>
-PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-SELECT ?sepakeuri ?label ?htmlDescription ?logo (COUNT(*) AS ?rank)
-WHERE {
-    { ?sepakeuri rdfs:label ?label .
-       FILTER (STRENDS(STR(?sepakeuri), ?suffix))
-    } .
-    OPTIONAL { ?sepakeuri sepake:htmlDescription ?htmlDescription } .
-    OPTIONAL { ?sepakeuri prov:wasDerivedFrom / foaf:logo ?logo } .
-    OPTIONAL { ?sepakeuri sepakemetrics:focushit []} . 
-}
-GROUP BY ?sepakeuri
-'''
+def _chunk(n, iterable):
+    args = [iter(iterable)] * n
+    return izip_longest(fillvalue = None, *args)
 
 def _document_from_sparql_result(sparql_result):
     fields = [search.HtmlField(name='label', value=sparql_result['label'])]
+
     if 'htmlDescription' in sparql_result:
         fields.append(search.HtmlField(name='description', value=sparql_result['htmlDescription']))
     if 'logo' in sparql_result:
         fields.append(search.AtomField(name='logo', value=sparql_result['logo']))
     return search.Document(doc_id = sparql_result['sepakeuri'], 
                            fields = fields,
-                           rank = sparql_result['rank'].value)
+                           rank = sparql_result['rank'])
     
 class Indexer(object):
     def __init__(self, graphid):
         self._index = search.Index(name = graphid)
         self._graph = Graph(store = connect(identifier = graphid))
-        
+        self._hits = Counter()
+        for (s, o) in self._graph.subject_objects(SEPAKEMETRICS.focushit):
+            self._hits[s] += 1
+    
+    def _dict_to_gae(self, d):
+        fields = [search.HtmlField(name='label', value=d['label'])]
+        fields.append(search.HtmlField(name='description', value=d['htmlDescription']))
+        if 'logo' in d:
+            fields.append(search.AtomField(name='logo', value=d['logo']))
+        return search.Document(doc_id = d['sepakeuri'], 
+                               fields = fields,
+                               rank = self._hits[d['sepakeuri']])
+
     def __iter__(self):
         #Step 1: Delete documents in index, in batches of 100
         while True:
@@ -54,10 +57,19 @@ class Indexer(object):
                 break
             # Delete the documents for the given ids from the Index.
             self._index.delete(document_ids)
-        #Step 2: Add documents from self._graph, in 16 batches
-        for suffix in [str(i) for i in range(10)] + [chr(i) for i in range(ord('a'), ord('g'))]:#Each of the 16 hex digits
-            index_now = self._graph.query(prepareQuery(_DOCUMENTS.replace('?suffix', '"{}"'.format(suffix))))#initBindings do not work with FILTER
-            dicts = [doc.asdict() for doc in index_now]
-            documents = [_document_from_sparql_result(d) for d in dicts if 'label' in d]
+        #Step 2: Add documents from self._graph, in batches of 200
+        for chunk in _chunk(200, self._graph.subject_objects(SEPAKE.htmlDescription)):
+            dicts = []
+            for pair in chunk:
+                if pair is not None:
+                    (s, o) = pair
+                    dicts.append({'sepakeuri':s, 'htmlDescription':o})
+            for d in dicts:
+                logo = self._graph.value(subject = d['sepakeuri'], predicate = PROV.wasDerivedFrom / FOAF.logo)
+                if logo is not None:
+                    d['logo'] = logo
+            for d in dicts:
+                d['label'] = self._graph.value(subject = d['sepakeuri'], predicate = RDFS.label, default = '')
+            documents = [self._dict_to_gae(d) for d in dicts]
             self._index.put(documents)
             yield len(documents)
